367756011c169841dd3d386bbcc451f902de97e4
pellyadolfo <pellyadolfo@yahoo.es>
2026-06-27 21:09:18 +0200

    updated supplySyncer

--- contracts/ISupplySyncer.sol
+++ contracts/ISupplySyncer.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.30;
 
 interface ISupplySyncer {
 
-    function sendSyncNodesTransaction(uint256 fromChain, address fromAddress, uint256 toChain, address toAddress, uint256 amount) external;
+    function sendSyncNodesTransaction(uint256 fromChain, uint256 toChain, uint256 amount) external;
 
-    function receiveSyncNodesTransaction(uint256 fromChain, address fromAddress, uint256 toChain, address toAddress, uint256 amount) external;
+    function receiveSyncNodesTransaction(uint256 fromChain, uint256 toChain, uint256 amount) external;
 
 }
--- test/contracts/MockSupplySyncer.t.sol
+++ test/contracts/MockSupplySyncer.t.sol
@@ -5,13 +5,13 @@ import "../../contracts/ISupplySyncer.sol";
 
 contract MockSupplySyncer is ISupplySyncer {
 
-    event SupplySynced(uint256 fromChain, address fromAddress, uint256 toChain, address toAddress, uint256 amount);
+    event SupplySynced(uint256 fromChain, uint256 toChain, uint256 amount);
 
-    function sendSyncNodesTransaction(uint256 fromChain, address fromAddress, uint256 toChain, address toAddress, uint256 amount) external {
+    function sendSyncNodesTransaction(uint256 fromChain, uint256 toChain, uint256 amount) external {
 
 		}
 
-    function receiveSyncNodesTransaction(uint256 fromChain, address fromAddress, uint256 toChain, address toAddress, uint256 amount) external {
+    function receiveSyncNodesTransaction(uint256 fromChain, uint256 toChain, uint256 amount) external {
 
 		}
 
