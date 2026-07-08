d350de4f9727ce34053b6118899c6865e6230c7f
pellyadolfo <pellyadolfo@yahoo.es>
2026-07-08 21:57:17 +0200

    updated interfaces

--- contracts/mock/MockedMessageRelayer.t.sol
+++ contracts/mock/MockedMessageRelayer.t.sol
@@ -5,9 +5,7 @@ import "../relayers/IMessageRelayer.sol";
 
 contract MockedMessageRelayer is IMessageRelayer {
 
-    function sendCrosschainMessage(uint256 destChain, address destAddress, uint256 amount) external override {
-
-
+    function sendCrosschainMessage(uint256 destChain, address destAddress, string calldata _message) external override {
         //emit MessageRelayed(sourceChainId, destinationChainId, sender, destination, payload);
 		}
 
--- contracts/relayers/IMessageRelayer.sol
+++ contracts/relayers/IMessageRelayer.sol
@@ -7,7 +7,7 @@ interface IMessageRelayer is IRelayer{
 
 	event MessageRelayed(uint256 indexed sourceChainId, uint256 indexed destinationChainId, address indexed sender, address destination, bytes payload);
 
-	function sendCrosschainMessage(uint256 destChain, address destAddress, uint256 amount) external;
+	function sendCrosschainMessage(uint256 destChain, address destAddress, string calldata _message) external;
 
 	function onCrosschainMessage(uint256 destChain, address destAddress, uint256 amount) external;
 
